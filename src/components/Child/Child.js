d5ff1cf32d61ec2ea9d99740d256a5af16d3d266
Robert Pliszka <robplisz@gmail.com>
2023-01-29 21:47:19 -0500

    Added comments to the code and deleted App component which wasnt needed

--- src/components/Child/Child.js
+++ src/components/Child/Child.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+// Child component
 class Child extends React.Component {
+  // Creating and setting state
   constructor(props) {
     super(props);
+    // Binding the handleChange()
     this.handleChange = this.handleChange.bind(this);
   }
 
+  //   Changes the name value
   handleChange(e) {
     const name = e.target.value;
     this.props.onChange(name);
@@ -14,7 +18,9 @@ class Child extends React.Component {
   render() {
     return (
       <div>
+        {/* Gets the name props */}
         <h1>Hey my name is {this.props.name}!</h1>
+        {/* Drop down menu */}
         <select onChange={this.handleChange}>
           <option value="Robert">Robert</option>
           <option value="Heidi">Heidi</option>
--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -1,13 +1,16 @@
 import React from "react";
+// Import Child component
 import Child from "../Child/Child";
 
 class Parent extends React.Component {
   constructor(props) {
     super(props);
+    // State initial value
     this.state = { name: "Robert" };
     this.changeName = this.changeName.bind(this);
   }
 
+  //   Function to change the value of name state which will be passed back from Child component
   changeName(newName) {
     this.setState({
       name: newName,
