6222d4ad2d212c667f15eaf13104d529babdc5de
Robert Pliszka <robplisz@gmail.com>
2023-01-29 21:30:53 -0500

    Updated the display compnent on the page

---
+++ src/components/Parent/Parent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Hello Test</h1>;
+  }
+}
+
+export default Parent;
